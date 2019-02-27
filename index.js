7387312f4495adb143ca4d53502ba64c7c1bb647
Juan <juan@villela.co>
2019-02-27 12:41:00 -0500

    Linting and fixes.

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 #!/usr/bin/env node
+
 const inquirer = require('inquirer');
 const fs = require('fs');
-const ncp = require('ncp').ncp;
+const {ncp} = require('ncp');
+
 const CHOICES = fs.readdirSync(`${__dirname}/templates`);
 const CURR_DIR = process.cwd();
 
 const QUESTIONS = [
   {
+    choices: CHOICES,
+    message: 'Choose your template',
     name: 'project-choice',
     type: 'list',
-    message: 'Choose your template',
-    choices: CHOICES
-  }
+  },
 ];
 
-inquirer.prompt(QUESTIONS)
-  .then(answers => {
-    const projectChoice = answers['project-choice'];
-    const templatePath = `${__dirname}/templates/${projectChoice}`;
-    ncp(templatePath, CURR_DIR, function (err) {
-      if (err) {
-        return console.error(err);
-      } else {
-        console.log('Done!');
-      }
-    });
+inquirer.prompt(QUESTIONS).then(answers => {
+  const projectChoice = answers['project-choice'];
+  const templatePath = `${__dirname}/templates/${projectChoice}`;
+  ncp(templatePath, CURR_DIR, err => {
+    if (err) {
+      console.error(`${err}`);
+    }
   });
+});
