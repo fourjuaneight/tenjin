6f82135db0ae272d3c7573042c455286f0857e25
Juan Villela <juan@villela.co>
2019-07-12 20:05:27 -0400

    Added error handling for Node readdirSync module.

--- index.js
+++ index.js
@@ -4,33 +4,37 @@ const { copyFile, readdirSync } = require('fs');
 const { join } = require('path');
 const { prompt } = require('inquirer');
 
-const sections = readdirSync(`${__dirname}/templates`);
-const CURR_DIR = process.cwd();
+const readDir = dir =>
+readdirSync(dir, (err, files) => {
+  if (err) throw err;
+  return files;
+});
 
-const questions = [
-  {
+const CURR_DIR = process.cwd();
+const sections = readDir(`${__dirname}/templates`);
+
+const templates = prompt({
     choices: sections,
     message: 'Choose a section',
     name: 'section',
     type: 'list',
-  },
-];
-
-const templates = prompt(questions).then(answers => {
-  const folder = answers.section;
-  const section = readdirSync(`${__dirname}/templates/${folder}`);
-  prompt({
-    choices: section,
-    message: 'Choose your template',
-    name: 'file',
-    type: 'list',
   }).then(answers => {
-    const templatePath = `${__dirname}/templates/${folder}/${answers.file}`;
-    const dest = join(CURR_DIR, answers.file);
-    copyFile(templatePath, dest, err => {
-      if (err) throw err;
+    const folder = answers.section;
+    const section = readDir(`${__dirname}/templates/${folder}`);
+
+    prompt({
+      choices: section,
+      message: 'Choose your template',
+      name: 'file',
+      type: 'list',
+    }).then(answers => {
+      const templatePath = `${__dirname}/templates/${folder}/${answers.file}`;
+      const dest = join(CURR_DIR, answers.file);
+
+      copyFile(templatePath, dest, err => {
+        if (err) throw err;
+      });
     });
   });
-});
 
 exports.default = templates;
