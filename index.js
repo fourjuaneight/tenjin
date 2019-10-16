fcc692a4700bf0d953fde2f8e4011c370265be65
Juan Villela <juan@villela.co>
2019-10-16 17:23:18 -0400

    Minor linting.

--- index.js
+++ index.js
@@ -5,36 +5,40 @@ const { join } = require('path');
 const { prompt } = require('inquirer');
 
 const readDir = dir =>
-readdirSync(dir, (err, files) => {
-  if (err) throw err;
-  return files;
-});
+  readdirSync(dir, (err, files) => {
+    if (err) {
+      throw err;
+    }
+    return files;
+  });
 
 const CURR_DIR = process.cwd();
 const sections = readDir(`${__dirname}/templates`);
 
 const templates = prompt({
-    choices: sections,
-    message: 'Choose a section',
-    name: 'section',
+  choices: sections,
+  message: 'Choose a section',
+  name: 'section',
+  type: 'list',
+}).then(answers => {
+  const folder = answers.section;
+  const section = readDir(`${__dirname}/templates/${folder}`);
+
+  prompt({
+    choices: section,
+    message: 'Choose your template',
+    name: 'file',
     type: 'list',
-  }).then(answers => {
-    const folder = answers.section;
-    const section = readDir(`${__dirname}/templates/${folder}`);
+  }).then(answer => {
+    const templatePath = `${__dirname}/templates/${folder}/${answer.file}`;
+    const dest = join(CURR_DIR, answer.file);
 
-    prompt({
-      choices: section,
-      message: 'Choose your template',
-      name: 'file',
-      type: 'list',
-    }).then(answers => {
-      const templatePath = `${__dirname}/templates/${folder}/${answers.file}`;
-      const dest = join(CURR_DIR, answers.file);
-
-      copyFile(templatePath, dest, err => {
-        if (err) throw err;
-      });
+    copyFile(templatePath, dest, err => {
+      if (err) {
+        throw err;
+      }
     });
   });
+});
 
 exports.default = templates;
