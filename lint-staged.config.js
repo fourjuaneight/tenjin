0d7b1f5be5347077d9a412c0c49691a24758d54e
Juan Villela <juan@villela.co>
2019-12-15 22:12:04 -0500

    Lint-staged now ignores config templates.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,11 @@
 // Static Testing
 // npm i -D husky lint-staged
+const micromatch = require('micromatch');
 
 module.exports = {
-  '*.+(js|css|ms)': ['prettier --write', 'git add'],
-  '*.js': ['eslint --fix'],
+  '*.js': files => {
+    const match = micromatch.not(files, 'configs/*.js');
+    return `eslint --fix ${match.join(' ')}`;
+  },
+  '*.+(js|css|ms)': ['prettier --write', 'git add'], // eslint-disable-line
 };
