47e974d8f8946fe00e06ad081538fa8e3f74aa7e
Juan Villela <juan@villela.co>
2020-06-11 13:28:49 -0400

    Added Gatsby specific critical CSS and Babel config.

---
+++ templates/gatsby/babel.config.js
@@ -0,0 +1,26 @@
+// Learn more about Babel:
+// https://babeljs.io/
+
+module.exports = api => {
+  const plugins = [
+    'module:@babel/plugin-proposal-class-properties',
+    'module:@babel/plugin-proposal-optional-chaining',
+    'macros',
+  ];
+  const presets = [
+    'module:babel-preset-gatsby',
+    [
+      'module:@babel/preset-env',
+      {
+        include: ['transform-arrow-functions'],
+      },
+    ],
+  ];
+
+  api.cache(false);
+
+  return {
+    plugins,
+    presets,
+  };
+};
