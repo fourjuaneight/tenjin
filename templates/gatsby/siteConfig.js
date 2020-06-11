bafd72872b51d3f1d8ce3e89aa153c78ae17bb86
Juan Villela <juan@villela.co>
2020-06-11 10:44:04 -0400

    Added Gatsby site config file.

---
+++ templates/gatsby/siteConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+  author: 'Big Pixel',
+  background: '#33BFBF',
+  description: '',
+  language: 'en',
+  ogLanguage: 'en_US',
+  shortName: 'TBF',
+  siteUrl: 'https://togglebooks-web-dev.herokuapp.com/',
+  theme: '#A212A5',
+  title: 'Toggle Book Factory',
+};
