f488d90d9e2891d85d01ab5e893729a38629172d
Juan Villela <juan@villela.co>
2020-12-27 18:20:23 -0500

    SW node snippet updates
    - Added SW file.
    - Changed caching helper to TS and renamed.

--- snippets/node/assetsToSW.ts
+++ snippets/node/assetsToSW.ts
@@ -1,12 +1,12 @@
-const { resolve } = require('path');
-const glob = require('glob').sync;
-const replace = require('replace-in-file');
+import { resolve } from 'path';
+import glob from 'glob';
+import replace from 'replace-in-file';
 
-const config = require('./siteConfig');
+import { siteUrl, title } from './siteConfig';
 
 // Glob options. Pass directory to search and files to ignore
 const cwd = resolve(__dirname, 'public');
-const ignore = ['sw.js'];
+const ignore: string[] = ['sw.js'];
 
 /**
  * Generate alphanumeric hash.
@@ -16,11 +16,11 @@ const ignore = ['sw.js'];
  *
  * @return {string}        alphanumeric hash
  */
-const makeHash = length => {
-  let result = '';
-  const characters =
+const makeHash = (length: number): string => {
+  let result: string = '';
+  const characters: string =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
+  const charactersLength: number = characters.length;
 
   for (let i = 0; i < length; i += 1) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -35,12 +35,28 @@ const makeHash = length => {
  *
  * @return {string} assets path list
  */
-const getFiles = () => {
-  const files = glob('**/*.{js,css,woff,woff2}', { cwd, ignore });
-  const filesList = files.map(toCache => `'/${toCache}'`).toString();
+const getFiles = (): string => {
+  const files: string[] = glob.sync('**/*.{js,css,woff,woff2}', {
+    cwd,
+    ignore,
+  });
+  const filesList: string = files.map(toCache => `'/${toCache}'`).toString();
 
   return filesList;
 };
+
+/**
+ * String to camel case.
+ * @function
+ *
+ * @param   {string} str space separate string
+ *
+ * @returns {string}     camelCased string
+ */
+const toCamelCase = (str: string): string =>
+  str
+    .toLowerCase()
+    .replace(/\s(.)/g, (match: string, group1: string) => group1.toUpperCase());
 
 (async () => {
   // find and replace options; add hash ID, files to cache array, and site base URL
@@ -49,12 +65,14 @@ const getFiles = () => {
     from: [
       /(const)\s(staticAssets)\s=\s?\[\];/g,
       /const\sversion\s=\s'';/g,
+      /const\cacheName\s=\s'';/g,
       /baseURL/g,
     ],
     to: [
       `const staticAssets = [${getFiles()}];`,
       `const version = '${makeHash(6)}';`,
-      `${config.siteUrl}`,
+      `const cacheName = '${toCamelCase(title)}';`,
+      `${siteUrl}`,
     ],
   };
 
@@ -64,6 +82,6 @@ const getFiles = () => {
     // eslint-disable-next-line no-console
     console.info('SW updated.');
   } catch (error) {
-    console.error('Error occurred:', error);
+    throw new Error('Error occurred:', error);
   }
 })();
