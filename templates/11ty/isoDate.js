e0c99393cc40591c4d312b0d836c948aa1042bb5
Juan Villela <juan@villela.co>
2020-12-27 18:35:19 -0500

    Removed 11ty templates.

--- templates/11ty/isoDate.js
+++
@@ -1,7 +0,0 @@
-module.exports = date => {
-  const raw = new Date(date);
-  const offset = raw.getTimezoneOffset() * 60000;
-  const newDate = new Date(raw - offset);
-
-  return newDate.toISOString();
-};
--- templates/11ty/zeroValue.js
+++
@@ -1,8 +0,0 @@
-module.exports = value => {
-  const toInt = parseInt(value, 10);
-  if (toInt === 0 || value === undefined) {
-    return '<1';
-  }
-
-  return value;
-};
