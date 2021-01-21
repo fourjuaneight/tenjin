b1fccf183ae1964322f1aeb5bd73d1843263ac33
Juan Villela <juan@villela.co>
2021-01-20 20:12:52 -0500

    Updated Nest eslint config.

--- templates/nest/.eslintrc.js
+++ templates/nest/.eslintrc.js
@@ -60,6 +60,11 @@ module.exports = {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "type|returns",
+      },
+    ],
   },
 };
