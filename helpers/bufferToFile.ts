e340ffd96b2c371522f8583f7994f6e01a63046e
Juan Villela <juan@villela.co>
2021-04-29 12:53:28 -0400

    Added fs helper methods.

---
+++ helpers/bufferToFile.ts
@@ -0,0 +1,22 @@
+import { promises } from "fs";
+
+const { writeFile } = promises;
+
+/**
+ * Save buffer as local file.
+ * @function
+ *
+ * @param   {Buffer} data media buffer to save
+ * @param   {string} name file name
+ *
+ * @returns {Promise<void>}
+ */
+const bufferToFile = async (data: Buffer, name: string): Promise<void> => {
+  try {
+    await writeFile(name, data);
+  } catch (error) {
+    throw new Error(`Saving buffer as file: \n ${error}`);
+  }
+};
+
+export default bufferToFile;
