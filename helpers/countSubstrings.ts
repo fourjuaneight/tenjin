d006e46dec981ae75fb4349a3dd03baae69d0ec3
Juan Villela <juan@villela.co>
2021-05-31 17:12:29 -0400

    Added string contents parsing helper.

---
+++ helpers/countSubstrings.ts
@@ -0,0 +1,26 @@
+/**
+ * Counts the occurrences of a substring in a given string.
+ * @function
+ * 
+ * @param str string to search
+ * @param searchValue value to search for
+ * @returns {number} found occurrences
+ */
+const countSubstrings = (str: string, searchValue: string): number => {
+  let count = 0;
+  let index = 0;
+
+  while (true) {
+    // look for searchValue in str
+    const round = str.indexOf(searchValue, index);
+
+    if (round !== -1) {
+      // increment if the value is found and update the inde
+      [count, index] = [count + 1, round + 1];
+    } else {
+      return count
+    };
+  }
+};
+
+export countSubstrings;
