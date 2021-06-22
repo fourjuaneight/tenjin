5aa04a653df081fbb909a45a51e9818f27ab9c4b
Juan Villela <juan@villela.co>
2021-06-22 18:19:26 -0400

    Minor font snippets syntax optimizations.

--- snippets/fonts.js
+++ snippets/fonts.js
@@ -2,19 +2,24 @@
  * Font loading via FontFace API to avoid FOUT.
  * After US charset subset files load, load full Latin chartset files.
  */
-if ('fonts' in document) {
+if ("fonts" in document) {
   const regular = new FontFace(
-    '<FONT>',
+    "<FONT>",
     'url(/fonts/<FONT>-Regular.woff2) format("woff2"), url(/fonts/<FONT>-Regular.woff) format("woff")'
   );
+  const medium = new FontFace(
+    "<FONT>",
+    'url(/fonts/<FONT>-Medium.woff2) format("woff2"), url(/fonts/<FONT>-Medium.woff) format("woff")',
+    { weight: "500" }
+  );
   const bold = new FontFace(
-    '<FONT>',
+    "<FONT>",
     'url(/fonts/<FONT>-Bold.woff2) format("woff2"), url(/fonts/<FONT>-Bold.woff) format("woff")',
-    { weight: '700' }
+    { weight: "700" }
   );
 
-  Promise.all([bold.load(), regular.load()]).then(fonts => {
-    fonts.forEach(font => {
+  Promise.all([regular.load(), medium.load(), bold.load()]).then((fonts) => {
+    fonts.forEach((font) => {
       document.fonts.add(font);
     });
   });
